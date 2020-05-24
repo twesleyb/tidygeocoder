07198e1ee81f7db5d97a3ae285888e29b7a5c595
twesleyb <tyler.w.bradshaw@duke.edu>
2020-05-23 23:24:55 -0400

    try using censuxy

--- tests/testthat/censusxy-test.R
+++ tests/testthat/censusxy-test.R
@@ -5,49 +5,38 @@ renv::load(getrd())
 # The censusxy package is designed to provide easy access to the U.S. Census
 # Bureau Geocoding Tools: https://geocoding.geo.census.gov/geocoder/ in R.
 
-### Motivation
 # The Census Bureau Geocoding Tools allow for both unlimited free geocoding as 
 # well as an added level of reproducibility compared to commercial geocoders. 
 # Many geospatial workflows involve a large quantity of addresses, hence our 
 # core focus is on batch geocoding.
 
-### Responsible Use
-# The U.S. Census Bureau makes their geocoding API available without any API key, 
+# NOTE: The U.S. Census Bureau makes their geocoding API available without any API key, 
 # and this package allows for virtually unlimited batch geocoding. Please use this 
 # package responsibly, as others will need use of this API for their research.
 
-### Installation
+# Installation
 #install.packages("censusxy")
 #devtools::install_github("slu-openGIS/censusxy")
-# Installation fails because of unmet units dependency.
+
+# NOTE: Installation fails because of unmet units dependency.
 # The 'sf' option in the central cxy_geocode function utilzes the
 # 'sf' package, which in turn relies upon the units library.
 # I've forked the repo and just commented out the offending line.
 # Try installing my fork:
 #devtools::install_github("twesleyb/census_xy")
 
-
-## Test data.
+# Load the test data.
+library(censusxy)
 data("stl_homicides")
 
-### Parsing Addresses
+# Parsing Addresses
 # Dataframe or alike object should contain seperate columns for:
 # NOTE: The postmastr package might be helpful if you need to get city/zip from
 # street address: https://github.com/slu-openGIS/postmastr).
-# * street address - required
+# * street address - required; the rest are optional.
 # * city
 # * state 
 # * zipcode
 
-### Batch Geocoding
-homicide_sf <- cxy_geocode(stl_homicides, address = street_address, 
-			   city = city, state = state, zip = postal_code, class = "sf")
-
-# Note, however, that it returns only matched addresses, including those approximated by street length. If there are unmatched addresses, they will be dropped from the output. Use `class = "dataframe"` to return all addresses, including those that are unmatched.
-
-# Output returned as an `sf` object can be previewed with a package like [`mapview`](https://cran.r-project.org/package=mapview):
-# mapview::mapview(homicide_sf)
-
-# With A For Loop
-addresses <- c("20 N Grand Blvd, St. Louis MO 63103", "3700 Lindell Blvd, St. Louis, MO 63108")
-geocodes <- lapply(addresses, cxy_oneline)
+# Batch Geocoding
+homicide_sf <- cxy_geocode(stl_homicides, street = 'street_address', city = 'city', state = 'state', zip = 'postal_code')
